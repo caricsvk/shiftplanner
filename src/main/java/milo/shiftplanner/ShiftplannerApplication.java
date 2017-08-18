8ff146cc62e058b4e6aec0a3a4a6ef0367ad7b64
Richard Casar <caricsvk@gmail.com>
2017-08-18 09:31:34 +0200

    spring boot auto generated files

---
+++ src/main/java/milo/shiftplanner/ShiftplannerApplication.java
@@ -0,0 +1,12 @@
+package milo.shiftplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftplannerApplication.class, args);
+	}
+}
